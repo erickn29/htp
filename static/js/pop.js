e4e565c9443f6688043d0b2394c5ae484c20ffd4
Yaroslav <arhdomain@gmail.com>
2022-06-05 17:28:02 +0300

    css update

--- static/js/pop.js
+++ static/js/pop.js
@@ -1,12 +1,24 @@
 function pop(){
-    if ($('.left-content').css('left') == '-300px'){
-        $('.left-content').css('left', '0px')
+    if ($('.left-slider').css('left') == '-300px'){
+        $('.left-slider').css('left', '0px')
         $('body').css('overflow', 'hidden')
         $('.cover').css('display', 'block')
     } else {
-        $('.left-content').css('left', '-300px')
+        $('.left-slider').css('left', '-300px')
         $('body').css('overflow', 'auto')
         $('.cover').css('display', 'none')
     }
     
 }
+
+$('#them-toggler').click(function(){
+    if ($('#css-theme').attr('href') == "static/css/dark.css"){
+        $('#css-theme').attr("href", "static/css/white.css")
+        $('#code-theme').attr("href", "static/css/hljs/default.min.css")
+
+    }else{
+        $('#css-theme').attr("href", "static/css/dark.css")
+        $('#code-theme').attr("href", "static/css/hljs/onedark.min.css")
+    }
+    
+})
