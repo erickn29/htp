13b69c8a619348c7626008a2f193597eae2cdb0a
yaroslav <arhdomain@gmail.com>
2022-07-19 23:04:19 +0300

    toggle theme

--- htp_app/views.py
+++ htp_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.shortcuts import redirect
 
 
 def index(request):
-    return render(request, 'htp_app/index.html', context={})
+    theme = request.COOKIES['theme']
+    return render(request, 'htp_app/index.html', context={'theme': theme})
+
+
+def set_cookie(request):
+    response = HttpResponse()
+    theme = request.GET['theme']
+    response.set_cookie('theme', theme)
+    return response
+
